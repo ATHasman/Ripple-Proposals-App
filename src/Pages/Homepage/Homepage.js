88a8b248746496f9503664ab29dd257eebb2fb08
Ethan Hasman <hasman1982@gmail.com>
2020-01-15 19:54:05 +0200

    creating & Rendering HomeNavBar Comp. in Homepage

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,36 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import HomeNavBar from '../../Components/HomeNavBar';
 import '../Homepage/Homepage.css';
-import logo from '../../Img/Ripple_Hand_logo_400x400 transparent.png'
-import { Container, Button, Navbar, Nav, Image } from 'react-bootstrap'
+import { Container, Button } from 'react-bootstrap';
 
 
 export default class Homepage extends Component {
     render() {
         return (
             <div className="Home">
-                 <Navbar bg="primary" variant="dark" expand="md">
-                    <Navbar.Brand href="/home"><Image src={logo} className="brand-logo" roundedCircle />Ripple</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="ml-auto">
-                            <Nav.Link href="#features">Features</Nav.Link>
-                            <Nav.Link href="#product">Product</Nav.Link>
-                            <Nav.Link href="#about">About</Nav.Link>
-                            <Nav.Link href="/login">Login</Nav.Link>
-                        </Nav>
-                        <Button bg="light" variant="success" >Sign Up Free</Button>
-                    </Navbar.Collapse>
-                    {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item href="#action/3.4">Logout</NavDropdown.Item>
-                    </NavDropdown> */}
-                </Navbar>
+                <HomeNavBar />
                 <Container>
-                <h1>Homepage</h1>
-                <Button>Click here</Button>
+                    <h1>Homepage</h1>
+                    <Button>Click here</Button>
                 </Container>
             </div>
         )

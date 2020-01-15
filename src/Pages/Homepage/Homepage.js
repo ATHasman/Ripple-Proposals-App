21337db91eebdf6ad09f2a04a97eab166fbe258a
Ethan Hasman <hasman1982@gmail.com>
2020-01-15 16:09:25 +0200

    Adding react-bootstrap

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import '../Homepage/Homepage.css';
+import { Container, Button } from 'react-bootstrap'
 
 
 export default class Homepage extends Component {
     render() {
         return (
             <div className="Home">
+                <Container>
                 <h1>Homepage</h1>
+                <Button>Click here</Button>
+                </Container>
             </div>
         )
     }

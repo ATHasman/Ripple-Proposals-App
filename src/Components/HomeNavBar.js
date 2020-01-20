799783cd11009b99ceeb6121deda31d5cb245ee8
Ethan Hasman <hasman1982@gmail.com>
2020-01-20 11:01:42 +0200

    edits on HomeNavbar Comp.

--- src/Components/HomeNavBar.js
+++ src/Components/HomeNavBar.js
@@ -3,11 +3,21 @@ import logo from '../Img/Ripple_Hand_logo_400x400 transparent.png'
 import { Button, Navbar, Nav, Image } from 'react-bootstrap'
 
 export default class HomeNavBar extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             
+        }
+
+       
+    }
+    
 
     render() {
         return (
             <div className="HomeNavBar">
-                 <Navbar bg="primary" variant="dark" expand="md">
+                 <Navbar bg="transparent" variant="light" expand="md">
                     <Navbar.Brand href="/"><Image src={logo} className="brand-logo" roundedCircle />Ripple</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
@@ -17,7 +27,7 @@ export default class HomeNavBar extends Component {
                             <Nav.Link href="/About">About</Nav.Link>
                             <Nav.Link href="#/login">Login</Nav.Link>
                         </Nav>
-                        <Button bg="light" variant="success" >Sign Up Free</Button>
+                        <Button bg="light" variant="success">Sign Up Free</Button>
                     </Navbar.Collapse>
                     {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>

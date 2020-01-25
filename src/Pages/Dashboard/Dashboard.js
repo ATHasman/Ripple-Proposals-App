744d9f2c37176c8abb6aaa88e988aaa9e3c9f7e5
Ethan Hasman <hasman1982@gmail.com>
2020-01-25 18:49:43 +0200

    Code Comments On Dashboard Page

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import '../Dashboard/Dashboard.css'
+import HomeNavBar from '../../Components/HomeNavBar'
+
 
 export default class Dashboard extends Component {
     constructor(props) {
@@ -11,8 +13,10 @@ export default class Dashboard extends Component {
     }
     
     render() {
+        
         return (
             <div className="Dashboard">
+                <HomeNavBar page="Dashboard" variant="light"/>
                 <h1>Dashboard Page</h1>
             </div>
         )

b1cfb624fd35b8fac238f96becd002dfe218df9d
Ethan Hasman <hasman1982@gmail.com>
2020-02-05 13:44:48 +0200

    Getting Templates from Parse - display in page

--- src/Pages/Templates/Templates.js
+++ src/Pages/Templates/Templates.js
@@ -2,18 +2,11 @@
 
 import React, { Component } from "react";
 import "../Templates/Templates.css";
-import {
-  Container,
-  Row,
-  Col,
-  InputGroup,
-  Accordion,
-  Card,
-  Jumbotron,
-} from "react-bootstrap";
+import {  Container,Row,Col,InputGroup,Jumbotron} from "react-bootstrap";
 import Parse from 'parse';
 import SearchBoxComp from "../../Components/SearchBoxComp";
 import TemplateModel from "../../Models/TemplateModel";
+import TemplateTileComp from "../../Components/TemplateTileComp";
 
 // Template Component Props:
 // activeUser={activeUser}
@@ -23,7 +16,7 @@ export default class Templates extends Component {
     super(props);
 
     this.state = {
-      templates: []
+      Templates:  []
     };
   }
 
@@ -31,52 +24,62 @@ export default class Templates extends Component {
     if (this.props.activeUser) {
         const templates = Parse.Object.extend('Templates');
         const query = new Parse.Query(templates);
-        
-        query.equalTo("serviceChargeQty", 1);
-        query.find().then((parseTemplate) => {
-          const Template = parseTemplate.map(parseTemplate => new TemplateModel(parseTemplate));
-          this.setState({Template});
+         // query.equalTo("serviceChargeQty", 1); //Query Condition segmentation
+
+        query.find().then((results) => {
+          const Templates = results.map(result => {
+            let temp = new TemplateModel(result);
+            console.log("each template inside map: ",temp);
+            return temp;
+          });
+          this.setState({Templates});
         }, (error) => {
-          console.error('Error while fetching Recipe', error);
+          console.error('Error while fetching Templates', error);
         });
     }
 }
 
 
   render() {
-    // Props extraction
+
     const { activeUser } = this.props;
+    const { Templates } = this.state;
+    console.log("state: "+Templates);
+
+    const TemplatesView = Templates.map(Temp => 
+      <Col lg={3} md={4}>
+          <TemplateTileComp Template={Temp} key={Templates.templateName}/>
+      </Col>)
+    console.log(TemplatesView);
 
     // Reading Templates Class from Parse (Extracting all Available Templates)
-
-    // 
-    // const Templates = Parse.Object.extend("Templates");
-    // const query = new Parse.Query(Templates);
-    // query.equalTo("serviceType", "A string");
-    // query.equalTo("dateModified", new Date());
-    // query.equalTo("templateName", "A string");
-    // query.equalTo("CoverText", "A string");
-    // query.equalTo("OverView", "A string");
-    // query.equalTo("WhyUs", "A string");
-    // query.equalTo("CoverUrl", "A string");
-    // query.equalTo("serviceItems", new Parse.Object("ServiceItems"));
-    // query.equalTo("WhyUsUrl", "A string");
-    // query.equalTo("serviceChargeQty", 1);
-    // query.equalTo("serviceCostUSD", 1);
-    // query.equalTo("serviceDelivery", new Date());
-    // query.equalTo("paymentType", "A string");
-    // query.equalTo("paymentVariable", "A string");
-    // query.find().then(
-    //   results => {
-    //     // You can use the "get" method to get the value of an attribute
-    //     // Ex: response.get("<ATTRIBUTE_NAME>")
-        
-    //     console.log("Templates found", results);
-    //   },
-    //   error => {
-    //     console.error("Error while fetching Templates", error);
-    //   }
-    // );
+        // const Templates = Parse.Object.extend("Templates");
+        // const query = new Parse.Query(Templates);
+        // query.equalTo("serviceType", "A string");
+        // query.equalTo("dateModified", new Date());
+        // query.equalTo("templateName", "A string");
+        // query.equalTo("CoverText", "A string");
+        // query.equalTo("OverView", "A string");
+        // query.equalTo("WhyUs", "A string");
+        // query.equalTo("CoverUrl", "A string");
+        // query.equalTo("serviceItems", new Parse.Object("ServiceItems"));
+        // query.equalTo("WhyUsUrl", "A string");
+        // query.equalTo("serviceChargeQty", 1);
+        // query.equalTo("serviceCostUSD", 1);
+        // query.equalTo("serviceDelivery", new Date());
+        // query.equalTo("paymentType", "A string");
+        // query.equalTo("paymentVariable", "A string");
+        // query.find().then(
+        //   results => {
+        //     // You can use the "get" method to get the value of an attribute
+        //     // Ex: response.get("<ATTRIBUTE_NAME>")
+            
+        //     console.log("Templates found", results);
+        //   },
+        //   error => {
+        //     console.error("Error while fetching Templates", error);
+        //   }
+        // );
 
     return (
       <div className="Templates">
@@ -100,29 +103,12 @@ export default class Templates extends Component {
             results={["Template 1", "Template 2", "Template 3"]}
           />
         </Jumbotron>
+        
+        {/* Map/Loop Rendering the Tiles of Templates in the Container Section */}
 
         <Container>
           <Row>
-            <Col>
-              <Accordion defaultActiveKey="0">
-                <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="0">
-                    Click me!
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="0">
-                    <Card.Body>Hello! I'm the body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-                <Card>
-                  <Accordion.Toggle as={Card.Header} eventKey="1">
-                    Click me!
-                  </Accordion.Toggle>
-                  <Accordion.Collapse eventKey="1">
-                    <Card.Body>Hello! I'm another body</Card.Body>
-                  </Accordion.Collapse>
-                </Card>
-              </Accordion>
-            </Col>
+              {TemplatesView}
           </Row>
         </Container>
       </div>

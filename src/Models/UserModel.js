162f2d8160142a961dbab13d3cd4f1e54781fbf7
Ethan Hasman <hasman1982@gmail.com>
2020-02-05 13:56:37 +0200

    User Model Comments

--- src/Models/UserModel.js
+++ src/Models/UserModel.js
@@ -9,3 +9,16 @@ export default class UserModel {
         this.industryField = parseModel.get("IndustryField")
     }
 }
+
+// {
+//     "objectId": "4BwpMWdCnm",
+//     "username": "A string",
+//     "email": "A string",
+//     "fname": "A string",
+//     "lname": "A string",
+//     "UserType": "A string",
+//     "IndustryField": "A string",
+//     "password": "#Password123",
+//     "createdAt": "2018-11-06T00:52:01.520Z",
+//     "updatedAt": "2018-11-06T00:52:04.713Z"
+//   }

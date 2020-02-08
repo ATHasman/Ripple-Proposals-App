8813508089727b75c33b4c04cea7e608971caa2d
Ethan Hasman <hasman1982@gmail.com>
2020-02-08 20:54:42 +0200

    Template Fetch using GET method (insted Query)

--- src/Pages/TemplateView/TemplateView.js
+++ src/Pages/TemplateView/TemplateView.js
@@ -1,51 +1,87 @@
-import React, { Component } from 'react'
-import TemplateModel from '../../Models/TemplateModel';
+/** @format */
+
+import React, { Component } from "react";
+import "../TemplateView/TemplateView.css";
+import TemplateModel from "../../Models/TemplateModel";
+import { Container, Jumbotron, InputGroup, Card } from "react-bootstrap";
+import Parse from "parse";
 
 // TemplateView Props:
 //      activeUser={activeUser}
 
 export default class TemplateView extends Component {
-    constructor(props) {
-        super(props)
-    
-        this.state = {
-            activeTemplateId : "",
-            Template : null
-        }
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-        //Extracting the Template ID from window URL . 
-        let id = window.location.href.split("/")[window.location.href.split("/").length - 1]   
-        // Async reading from Parse Templates DB
-        const templates = Parse.Object.extend('Templates');
-        const query = new Parse.Query(templates);
-        //Query for Template.id
-        query.equalTo("id", id); 
-        query.find().then((results) => {
-            console.log('Templates found', results);
-            let templateResult = new TemplateModel(results)
-            console.log("Model from parse result: ",templateResult)
-            // Updating State with Template_id; Extracted Template from Parse Template DB
-            this.setState({
-                activeTemplateId : id,
-                Template: templateResult
-            })
-          }, (error) => {
-            console.error('Error while fetching Templates', error);
+    this.state = {
+      activeTemplateId: window.location.href.split("/")[
+        window.location.href.split("/").length - 1
+      ],
+      Template: {}
+    };
+  }
+
+  componentDidMount() {
+    // Repeating Initial Constructor Template Page View - Updating new State in case of change
+    //Extracting the Template ID from window URL (String.Split > last subdomain soffix after "/"  ) .
+    let tempId = window.location.href.split("/")[
+      window.location.href.split("/").length - 1
+    ];
+    // Async reading from Parse Templates DB
+    if (this.props.activeUser) {
+      const parseTemplates = Parse.Object.extend("Templates");
+      const query = new Parse.Query(parseTemplates);
+      query.get(tempId).then(
+        result => {
+          console.log("Templates found", result);
+          // setResult in templateResult
+          let templateResultModel = new TemplateModel(result);
+          console.log("Model from parse result: ", templateResultModel);
+          // Updating State with Template_id; Extracted Template from Parse Template DB
+          this.setState({
+            activeTemplateId: tempId,
+            Template: templateResultModel
           });
-
+        },
+        error => {
+          console.error("Error while fetching Templates", error);
+        }
+      );
     }
-    
-    render() {
+  }
 
-        const { activeUser } = this.props;
-        const { activeTemplateId , Template } = this.state;
-        
-        return (
-            <div classname="TemplateView">
-                
-            </div>
-        )
-    }
+  render() {
+    const { activeUser } = this.props;
+    const { activeTemplateId, Template } = this.state;
+
+    return (
+      <div className="TemplateView">
+        <Jumbotron className="templateView-JT">
+          <h1>{Template.templateName}</h1>
+          <InputGroup className="mb-3">
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                Industry : {activeUser.industryField}
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+          </InputGroup>
+          <p>You can Modify Customize and Edit your content template....</p>
+          <small>Template ID : {activeTemplateId}</small>
+        </Jumbotron>
+
+        <Container>
+          <Card>
+            <Card.Header>
+            <h4>{Template.serviceType}</h4>
+            <p>{activeUser.userType}</p> 
+            </Card.Header>
+            <Card.Img src={Template.CoverUrl} alt={Template.serviceType} />
+            <Card.Body>
+
+            </Card.Body>
+          </Card>
+        </Container>
+      </div>
+    );
+  }
 }

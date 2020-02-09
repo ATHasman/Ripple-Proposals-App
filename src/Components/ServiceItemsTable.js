7f5f40106117fec885ab10cc44d12a7fecb77869
Ethan Hasman <hasman1982@gmail.com>
2020-02-09 12:19:39 +0200

    Creating ServiceItems Component

---
+++ src/Components/ServiceItemsTable.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+
+// <ServiceItemsTable> Props:
+//      activeUser={activeUser} Obj
+//      ServiceItems={ServiceItems} Array of ServiceItemsModel 
+
+export default class ServiceItemsTable extends Component {
+    render() {
+        return (
+            <div classsName="ServiceItemsTable">
+                
+            </div>
+        )
+    }
+}

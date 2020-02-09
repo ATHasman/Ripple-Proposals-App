9b684413bbe47d3f5d2ac57ece0a3fb7fd593a20
Ethan Hasman <hasman1982@gmail.com>
2020-02-09 13:23:20 +0200

    Done : ServiceItems  Comp. in TempView Section

--- src/Components/ServiceItemsTable.js
+++ src/Components/ServiceItemsTable.js
@@ -1,16 +1,70 @@
-import React, { Component } from 'react'
+/** @format */
 
+import React, { Component } from "react";
+import { Table } from "react-bootstrap";
 
 // <ServiceItemsTable> Props:
 //      activeUser={activeUser} Obj
-//      ServiceItems={ServiceItems} Array of ServiceItemsModel 
+//      ServiceItems={ServiceItems} Array of ServiceItemsModel
 
 export default class ServiceItemsTable extends Component {
-    render() {
-        return (
-            <div classsName="ServiceItemsTable">
-                
-            </div>
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+
+    console.log("ServiceItems props Constructor", this.props.ServiceItems );
+  }
+
+  render() {
+    const { activeUser, ServiceItems } = this.props;
+    let ServiceRowItems = [];
+    ServiceItems.map((item, index) => {
+        ServiceRowItems.push(
+        <tr>
+            <td>{index+1}</td>
+            <td>{item.serviceName}</td>
+            <td>{item.serviceDescription}</td>
+            <td>{item.serviceChargeBy}</td>
+            <td>{item.serviceChargeQty}</td>
+            <td>{item.serviceChargePrice}</td>
+            <td>{item.serviceChargeQty*item.serviceChargePrice}</td>
+        </tr>
         )
-    }
+    });
+    console.log(ServiceRowItems);
+
+    return (
+      <div classsName="ServiceItemsTable">
+        <Table responsive>
+          <thead>
+            <tr>
+              <th>Item#</th>
+              <th>Service Name</th>
+              <th>Description</th>
+              <th>Charge By</th>
+              <th>Charge Qty.</th>
+              <th>Price ($USD)</th>
+              <th>Invest</th>
+            </tr>
+          </thead>
+          <tbody>
+              {ServiceRowItems}
+          </tbody>
+        </Table>
+      </div>
+    );
+  }
 }
+
+// {
+//     "serviceDescription": "A string",
+//     "serviceName": "A string",
+//     "serviceField": "A string",
+//     "serviceType": "A string",
+//     "serviceChargeBy": "A string",
+//     "serviceChargePrice": 1,
+//     "isProjectBased": true,
+//     "serviceChargeQty": 1
+//   }

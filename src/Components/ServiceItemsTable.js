ae0abf3625f79668ec05b5f3fbcb04ddf3ed3267
Ethan Hasman <hasman1982@gmail.com>
2020-02-09 20:46:33 +0200

    fix Warnings ((UniqueKey) + ServiceCost( ) method

--- src/Components/ServiceItemsTable.js
+++ src/Components/ServiceItemsTable.js
@@ -13,43 +13,54 @@ export default class ServiceItemsTable extends Component {
     this.state = {
 
     };
-
     console.log("ServiceItems props Constructor", this.props.ServiceItems );
+
+    this.calsServiceCost = this.calsServiceCost.bind(this)
   }
 
+  calsServiceCost() {
+    const {ServiceItems} = this.props;
+    const defaultProjectService = 10000;
+    if (!ServiceItems.isProjectBased) {
+      return ((ServiceItems.serviceChargeQty)*(ServiceItems.serviceChargePrice));
+    } else {
+      return (defaultProjectService)
+    };
+  }
+  
   render() {
     const { activeUser, ServiceItems } = this.props;
     let ServiceRowItems = [];
     ServiceItems.map((item, index) => {
         ServiceRowItems.push(
-        <tr>
+        <tr key={`${activeUser.id}-${item.id}-serviceItem${index}`} >
             <td>{index+1}</td>
-            <td>{item.serviceName}</td>
-            <td>{item.serviceDescription}</td>
-            <td>{item.serviceChargeBy}</td>
-            <td>{item.serviceChargeQty}</td>
-            <td>{item.serviceChargePrice}</td>
-            <td>{item.serviceChargeQty*item.serviceChargePrice}</td>
-        </tr>
-        )
+            <td key={`${activeUser.id}-${item.id}-serviceName`}>{item.serviceName}</td>
+            <td key={`${activeUser.id}-${item.id}-serviceDescription`}>{item.serviceDescription}</td>
+            <td key={`${activeUser.id}-${item.id}-serviceChargeBy`}>{item.serviceChargeBy}</td>
+            <td key={`${activeUser.id}-${item.id}-serviceChargeQty`}>{item.serviceChargeQty}</td>
+            <td key={`${activeUser.id}-${item.id}-serviceChargePrice`}>{item.serviceChargePrice}</td>
+            <td key={`${activeUser.id}-${item.id}-serviceItemCost`}>{item.calsServiceCost}</td>
+        </tr>       );
+        return (ServiceRowItems);
     });
     console.log(ServiceRowItems);
 
     return (
-      <div classsName="ServiceItemsTable">
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Item#</th>
-              <th>Service Name</th>
-              <th>Description</th>
-              <th>Charge By</th>
-              <th>Charge Qty.</th>
-              <th>Price ($USD)</th>
-              <th>Invest</th>
+      <div className="ServiceItemsTable">
+        <Table key={`${activeUser.id}-${ServiceItems.id}-serviceTable`} responsive>
+          <thead key={`${activeUser.id}-${ServiceItems.id}-thead`}>
+            <tr key={`${activeUser.id}-${ServiceItems.id}-tr`}>
+              <th key={`${activeUser.id}-${ServiceItems.id}-#`}>Item#</th>
+              <th key={`${activeUser.id}-${ServiceItems.id}-Name`}>Service Name</th>
+              <th key={`${activeUser.id}-${ServiceItems.id}-Description`}>Description</th>
+              <th key={`${activeUser.id}-${ServiceItems.id}-ChargeBy`}>Charge By</th>
+              <th key={`${activeUser.id}-${ServiceItems.id}-Qty`}>Charge Qty.</th>
+              <th key={`${activeUser.id}-${ServiceItems.id}-Price`}>Price ($USD)</th>
+              <th key={`${activeUser.id}-${ServiceItems.id}-Cost`}>Invest</th>
             </tr>
           </thead>
-          <tbody>
+          <tbody key={`${activeUser.id}-${ServiceItems.id}-tbody`}>
               {ServiceRowItems}
           </tbody>
         </Table>

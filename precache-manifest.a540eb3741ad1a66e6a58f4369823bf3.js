51bb6030175ce50e215a1cde8f744d1d42efb4c4
Ethan Hasman <hasman1982@gmail.com>
2020-02-10 09:45:18 +0200

    Updates

---
+++ precache-manifest.a540eb3741ad1a66e6a58f4369823bf3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5cb063b76582e41afead71c46e0a23c",
+    "url": "/ripple-proposals-app/index.html"
+  },
+  {
+    "revision": "dbd65455cda9eb91ae39",
+    "url": "/ripple-proposals-app/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "945f7532648ce5375716",
+    "url": "/ripple-proposals-app/static/css/main.fda4a346.chunk.css"
+  },
+  {
+    "revision": "dbd65455cda9eb91ae39",
+    "url": "/ripple-proposals-app/static/js/2.ff96ec96.chunk.js"
+  },
+  {
+    "revision": "1a3ba1185bf697297fa4b180bfc93f40",
+    "url": "/ripple-proposals-app/static/js/2.ff96ec96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "945f7532648ce5375716",
+    "url": "/ripple-proposals-app/static/js/main.99f7f831.chunk.js"
+  },
+  {
+    "revision": "574e3d2b3ddde2b3ab75",
+    "url": "/ripple-proposals-app/static/js/runtime-main.93103a12.js"
+  },
+  {
+    "revision": "1c6e22c789c201ca0b5fd4d7754c1403",
+    "url": "/ripple-proposals-app/static/media/Log-aqua-ripple-bg-.1c6e22c7.png"
+  },
+  {
+    "revision": "f3a352cbc43c62eb6933cc6ac2dd0042",
+    "url": "/ripple-proposals-app/static/media/RIpple_blue_dark_bg_1920x1080.f3a352cb.jpg"
+  },
+  {
+    "revision": "811b85e4ad1a194c22f1e0cffb375a10",
+    "url": "/ripple-proposals-app/static/media/Ripple_Hand_logo_400x400 transparent.811b85e4.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ripple-proposals-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
